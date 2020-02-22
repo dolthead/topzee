b07df015a622fee5671bfcee1ffe9c67bf4bfd9f
T Hale <tdhale@verisk.com>
2020-02-22 11:59:34 -0700

    attributions and fix test

--- src/app/about/about.page.spec.ts
+++ src/app/about/about.page.spec.ts
@@ -2,26 +2,27 @@ import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AboutPage } from './about.page';
+import { ModalController, AngularDelegate, Platform } from '@ionic/angular';
 
 describe('AboutPage', () => {
-  let component: AboutPage;
-  let fixture: ComponentFixture<AboutPage>;
+    let component: AboutPage;
+    let fixture: ComponentFixture<AboutPage>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ AboutPage ],
-      schemas: [CUSTOM_ELEMENTS_SCHEMA],
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [AboutPage],
+            schemas: [CUSTOM_ELEMENTS_SCHEMA],
+            providers: [Platform, ModalController, AngularDelegate],
+        }).compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(AboutPage);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(AboutPage);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
