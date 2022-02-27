4556dfc391166dc894959e687bdd878314e12497
Todd H <github@simplybest.com>
2022-02-27 13:15:53 -0700

    add e2e test and fix sonar issues

---
+++ cypress/integration/load-all-pages.spec.ts
@@ -0,0 +1,28 @@
+
+const pageList = [
+    { 
+        pageTitle: 'Topzee', url: '/home',
+        elements: ['ion-img', 'ion-button' ],
+    },
+    { 
+        pageTitle: 'Topzee', url: '/GameScreen',
+        elements: ['ion-img', 'ion-button' ],
+    },
+];
+
+describe('Topzee Ionic app', () => {
+    it('loads dashboard page without errors', () => {
+        cy.visit('/');
+        cy.url().should('include', '/home');
+    });
+
+    pageList.forEach(page => {
+        it(`loads ${page.url} with title and no errors`, () => {
+            cy.visit(page.url);
+            cy.get('ion-title').should('contain', page.pageTitle);
+            if (page.elements) {
+                page.elements.forEach(el => cy.get(el).should('exist'));
+            }
+        });
+    });
+});
